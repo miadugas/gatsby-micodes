be5f6fd9190e75fd669fa9f70e24b354ab5d81db
Mia Morticia <miamorticia@Mias-MacBook-Pro.local>
2021-03-04 15:44:46 -0700

    first commit

---
+++ src/constants/services.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { FaCode, FaSketch, FaApple } from "react-icons/fa"
+export default [
+  {
+    id: 1,
+    icon: <FaCode className="service-icon" />,
+    title: "web development",
+    // Everywhere & Every Device, Your Site Should Flow Seamlessly
+    text: `Like the perfect cocktail should still look and taste perfect no matter the size of the glass you serve it in. I feel the same way about the sites I mix up. From shot to pitcher, smartphone to desktop, your site should look and function seamlessly.`,
+  },
+  {
+    id: 2,
+    icon: <FaSketch className="service-icon" />,
+    title: "web design",
+    // It’s All About the User Experience
+    text: `The reason you go to your favorite bar isn’t because of the low prices or convenient location; you go there because of the overall experience. I take that same approach with the apps and websites I create. I focus on overall usability, ease of use, and the interaction between the user and product. The result is engagement, social interaction, and sales.`,
+  },
+  {
+    id: 3,
+    icon: <FaApple className="service-icon" />,
+    title: "app design",
+    // From A Sketch On A Napkin To A Full Blown App
+    text: `Tap into the ability to bring your mobile app idea to life. I have proven experience in the world of app design, app development, and testing ... Apple or Android `,
+  },
+]
---
+++ src/constants/socialLinks.js
@@ -0,0 +1,51 @@
+import React from "react"
+import {
+  FaLinkedin,
+  FaGithubSquare,
+  FaDribbbleSquare,
+  FaCodepen,
+  FaTwitterSquare,
+} from "react-icons/fa"
+
+const data = [
+  {
+    id: 1,
+    icon: <FaGithubSquare className="social-icon"></FaGithubSquare>,
+    url: "https://github.com/miadugas",
+  },
+  {
+    id: 2,
+    icon: <FaLinkedin className="social-icon"></FaLinkedin>,
+    url: "https://www.linkedin.com/in/miadugas/",
+  },
+  {
+    id: 3,
+    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
+    url: "https://dribbble.com/MiaInBatcountry",
+  },
+  {
+    id: 4,
+    icon: <FaCodepen className="social-icon"></FaCodepen>,
+    url: "https://codepen.io/miadugas",
+  },
+  {
+    id: 5,
+    icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
+    url: "https://twitter.com/miacodes",
+  },
+]
+const links = data.map(link => {
+  return (
+    <li key={link.id}>
+      <a href={link.url} className="social-link">
+        {link.icon}
+      </a>
+    </li>
+  )
+})
+
+export default ({ styleClass }) => {
+  return (
+    <ul className={`social-links ${styleClass ? styleClass : ""}`}>{links}</ul>
+  )
+}
